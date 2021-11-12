3b06218e4b8a7acc21c4e36a93a0313218487ca4
Dou-Meishi <doumeishid@gmail.com>
2021-11-12 20:06:23 +0800

    Update init.el

--- init.el
+++ init.el
@@ -29,30 +29,28 @@
  '(confirm-nonexistent-file-or-buffer nil)
  '(cursor-in-non-selected-windows 'hollow)
  '(cursor-type '(bar . 4))
- '(elpy-rpc-python-command "python3")
  '(font-use-system-font t)
  '(global-company-mode t)
  '(global-hl-line-mode t)
  '(global-linum-mode t)
+ '(gnus-inhibit-startup-message t)
  '(indent-tabs-mode nil)
  '(inhibit-startup-buffer-menu t)
  '(inhibit-startup-echo-area-message "dou")
  '(inhibit-startup-screen t)
  '(initial-major-mode 'org-mode)
- '(initial-scratch-message nil)
+ '(initial-scratch-message "")
  '(ispell-dictionary "american")
  '(org-bullets-bullet-list '("☯" "✿" "◉" "○"))
  '(org-cycle-separator-lines 1)
  '(org-ellipsis "↺")
  '(org-export-with-section-numbers nil)
- '(org-hide-block-startup t)
  '(org-hide-emphasis-markers t)
  '(org-html-doctype "html5")
  '(org-html-head
    "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://gongzhitaao.org/orgcss/org.css\"/>
 <link rel=\"stylesheet\" type=\"text/css\" href=\"https://dou-meishi.github.io/orgcss/worg.css\"/>
 <link rel=\"stylesheet\" type=\"text/css\" href=\"https://dou-meishi.github.io/orgcss/dou-org.css\"/>")
- '(org-html-head-include-default-style nil)
  '(org-html-html5-fancy t)
  '(org-html-mathjax-options
    '((path "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js")
@@ -64,8 +62,7 @@
  '(org-html-postamble t)
  '(org-html-postamble-format '(("en" "<p class=\"date\">Last modified: %C</p>")))
  '(org-src-fontify-natively t)
- '(org-startup-folded 'content)
- '(org-startup-indented t)
+ '(org-startup-folded t)
  '(org-todo-keyword-faces
    '(("TODO" :foreground "#dc322f" :weight extra-bold :family "微软雅黑")
      ("FOLLOW" :foreground "DarkOrange" :weight bold :family "微软雅黑")
@@ -75,23 +72,25 @@
  '(org-todo-keywords
    '((sequence "TODO(t)" "FOLLOW(f)" "WORKING(w)" "CONCLUDE(c)" "DONE(d)")))
  '(package-selected-packages
-   '(solarized-theme yasnippet-snippets markdown-mode company-jedi elpy org-bullets dash-functional dash csv-mode))
+   '(cnfonts solarized-theme yasnippet-snippets markdown-mode company-jedi elpy org-bullets dash-functional dash csv-mode))
  '(read-buffer-completion-ignore-case t)
  '(read-file-name-completion-ignore-case t)
  '(scroll-conservatively 10000)
  '(scroll-margin 3)
  '(scroll-step 1)
+ '(show-paren-mode t)
  '(tab-width 4)
  '(tool-bar-mode nil)
+ '(visible-bell t)
  '(yas-global-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :extend nil :overline nil :underline nil :slant normal :weight semi-bold :height 158 :width normal :foundry "????" :family "文泉驿等宽微米黑"))))
+ '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :extend nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "outline" :family "文泉驿等宽微米黑"))))
  '(fixed-pitch ((t (:family "文泉驿等宽微米黑"))))
- '(italic ((t (:slant italic :family "Fira Sans"))))
+ '(italic ((t (:slant italic :family "Bookman Old Style"))))
  '(org-block ((t (:family "DejaVu Sans Mono"))))
  '(org-block-background ((t (:family "DejaVu Sans Mono"))))
  '(org-block-begin-line ((t (:inherit nil :foreground "#2aa198" :overline t :underline nil :weight bold))))
@@ -99,7 +98,7 @@
  '(org-code ((t (:foreground "#d33682" :family "DejaVu Sans Mono"))))
  '(org-default ((t (:inherit default))))
  '(org-document-title ((t (:inherit default :weight bold :font "微软雅黑" :height 2.0 :underline nil))))
- '(org-level-1 ((t (:inherit default :weight bold :font "微软雅黑" :height 1.75))))
+ '(org-level-1 ((t (:inherit default :weight bold :family "微软雅黑" :height 1.75))))
  '(org-level-2 ((t (:inherit default :weight bold :font "微软雅黑" :height 1.5))))
  '(org-level-3 ((t (:inherit default :weight bold :font "微软雅黑" :height 1.25))))
  '(org-level-4 ((t (:inherit default :weight bold :font "微软雅黑" :height 1.1))))
@@ -107,9 +106,9 @@
  '(org-level-6 ((t (:inherit default :weight bold :font "微软雅黑"))))
  '(org-level-7 ((t (:inherit default :weight bold :font "微软雅黑"))))
  '(org-level-8 ((t (:inherit default :weight bold :font "微软雅黑"))))
- '(org-table ((t (:foreground "#859900" :height 120 :family "Noto Sans Mono CJK SC"))))
+ '(org-table ((t (:foreground "#859900" :family "等距更纱黑体 SC"))))
  '(org-verbatim ((t (:foreground "#d33682" :family "DejaVu Sans Mono"))))
- '(variable-pitch ((t (:family "微软雅黑")))))
+ '(variable-pitch ((t (:font "微软雅黑")))))
 
 
 ;; beautify org mode
@@ -155,11 +154,17 @@ If the new path's directories does not exist, create them."
 (setq completion-ignore-case t)
 
 
-;; python mode
-;; (defun enable-jedi ()
-;;   (add-to-list 'company-backends 'company-jedi))
-;; (defun toggle-off-idle-company ()
-;;   (setq company-idle-delay 1000.0))
-;; (add-hook 'python-mode-hook 'enable-jedi)
-;; (elpy-enable)
-;; (setq elpy-rpc-backend "jedi")
+;; on windows
+(set-language-environment "UTF-8")
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
+
+;; set font for character U226F
+(set-fontset-font "fontset-default" '(#x262f . #x262f) "等距更纱黑体 SC")
+(set-fontset-font "fontset-default" '(#x25c9 . #x25c9) "等距更纱黑体 SC")
+(set-fontset-font "fontset-default" '(#x273f . #x273f) "等距更纱黑体 SC")
+(set-fontset-font "fontset-default" '(#x21ba . #x21ba) "等距更纱黑体 SC")
+(set-fontset-font "fontset-default" 'han (font-spec :name "文泉驿等宽微米黑" :weight 'bold))
